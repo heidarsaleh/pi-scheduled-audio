b964bd911bbafcfd6ab6d8b052b95b41594c7dee
Heidar <heidar.m@live.com>
2018-04-02 18:53:00 +0200

    push notifications

--- index.js
+++ index.js
@@ -13,12 +13,6 @@ console.log('Time is now: ' + moment(Date.now()).format('LLLL'))
 var p = new Push( {
 	user: c.user,
 	token: c.token
-	// httpOptions: {
-	//   proxy: process.env['http_proxy'],
-	//},
-	// onerror: function(error) {},
-	// update_sounds: true // update the list of sounds every day - will
-	// prevent app from exiting.
   })
 
 schedule.scheduleJob('0 7 * * FRI', () => {
@@ -32,26 +26,20 @@ s.forEach(day => {
 
 		console.log('Scheduling an alarm at: ' + date.format('LLLL'))
 
-		var msg = {
-			// These values correspond to the parameters detailed on https://pushover.net/api
-			// 'message' is required. All other values are optional.
-			message: t[3] || 'PI Alarm!',	// required
-			title: t[4] || 'PI Wanna Say something!'/*,
-			sound: 'magic',
-			device: 'devicename',
-			priority: 1*/
-		  }
-		   
-		  p.send( msg, function( err, result ) {
-			if ( err ) {
-			  throw err
-			}
-		   
-			console.log( result )
-		  })
-
 		schedule.scheduleJob(date.toDate(), () => {
 			player.play(day.audio, (err) => err ? console.log('Error!', err) : console.log('Succeeded!'))
+			var msg = {
+				message: t[3] || 'PI Alarm!',
+				title: t[4] || 'PI Wanna Say something!'
+			  }
+			   
+			  p.send( msg, function( err, result ) {
+				if ( err ) {
+				  throw err
+				}
+			   
+				console.log( result )
+			  })
 		})
 	})
 })
