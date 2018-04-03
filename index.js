1858c6d05e84d8ac08e97f3f007232571f97b94f
Heidar <heidar.m@live.com>
2018-04-03 20:11:37 +0200

    kill audio endpoint

--- index.js
+++ index.js
@@ -4,11 +4,42 @@ const player = require('play-sound')(opts = {})
 const schedule = require('node-schedule')
 const moment = require('moment-timezone')
 const Push = require('pushover-notifications')
+const express = require('express')
 
 const s = require('./schedule')
 const c = require('./config')
+const app = express()
+
+var os = require('os');
+var ifaces = os.networkInterfaces();
+
+Object.keys(ifaces).forEach(function (ifname) {
+  var alias = 0;
+
+  ifaces[ifname].forEach(function (iface) {
+    if ('IPv4' !== iface.family || iface.internal !== false) {
+      // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+      return;
+    }
+
+    if (alias >= 1) {
+      // this single interface has multiple ipv4 addresses
+      console.log(ifname + ':' + alias, iface.address);
+    } else {
+      // this interface has only one ipv4 adress
+      console.log(ifname, iface.address);
+    }
+    ++alias;
+  });
+});
 
 console.log('Time is now: ' + moment(Date.now()).format('LLLL'))
+
+let activeAudio;
+const killAudio = () => {
+	try { 
+		activeAudio && activeAudio.kill ? activeAudio.kill() : undefined 
+	} catch(err) {console.error('failed to kill audio', err)}}
 
 var p = new Push( {
 	user: c.user,
@@ -16,13 +47,14 @@ var p = new Push( {
   })
 
 schedule.scheduleJob('0 7 * * FRI', () => {
-	player.play('audio/f.mp3', (err) => err ? console.log('Error!', err) : console.log('Succeeded!'))
+	killAudio()
+	activeAudio = player.play('audio/f.mp3', (err) => err ? console.log('Error!', err) : console.log('Succeeded!'))
 })
 
 schedule.scheduleJob('0 20 * * 1-5', () => {
-	player.play('audio/goodnight.mp3', (err) => err ? console.log('Error!', err) : console.log('Succeeded!'))
+	killAudio()
+	activeAudio = player.play('audio/goodnight.mp3', (err) => err ? console.log('Error!', err) : console.log('Succeeded!'))
 })
-
 
 s.forEach(day => {
 	day.times.forEach(t => {
@@ -32,7 +64,8 @@ s.forEach(day => {
 		console.log('Scheduling an alarm at: ' + date.format('LLLL'))
 
 		schedule.scheduleJob(date.toDate(), () => {
-			player.play(day.audio, (err) => err ? console.log('Error!', err) : console.log('Succeeded!'))
+			killAudio()
+			activeAudio = player.play(day.audio, (err) => err ? console.log('Error!', err) : console.log('Succeeded!'))
 			var msg = {
 				message: t[3] || 'PI Alarm!',
 				title: t[4] || 'PI Wanna Say something!'
@@ -43,11 +76,15 @@ s.forEach(day => {
 				  throw err
 				}
 			   
-				console.log( result )
+				console.log(result)
 			  })
 		})
 	})
 })
+
+app.get('/killAudio', (req, res) => killAudio() || res.send('Sound has been killed successfully!'))
+
+app.listen(3030, () => console.log('app listens on port 3030'))
 
 setInterval(() => {
 	console.log('I\'m alive!')
