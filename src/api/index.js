c46377430346aa38f8e3d2c7254c4364d51157a2
Heidar <heidar.m@live.com>
2018-04-07 20:52:49 +0200

    get-big

--- src/api/index.js
+++ src/api/index.js
@@ -5,10 +5,14 @@ import {kill, play} from '../lib/audio-manager'
 
 const app = express()
 app.get('/stop-audio', (req, res) => kill() || res.send('Stop audio has been triggered.'))
-app.get('/get-big', (req, res) => {
-    console.log('playing Get Big!');
-    play('audio/getbig.mp3')
-    res.send('Get Big!')
+app.get('/get-big', async (req, res) => {
+    try {
+        console.log('playing Get Big!');
+        await play('audio/getbig.mp3')
+        res.send('Get Big!')
+    } catch(err) {
+        res.send('Get Big Failed!')
+    }
 })
 app.listen(c.port, () => console.log(`app listens on port ${c.port}`))
 
